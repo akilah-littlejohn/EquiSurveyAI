fcc9551e24ca412015e821d18c307ed63f8277d6
Akiah <akilahlittlejohn@gmail.com>
2024-10-15 08:45:42 -0500

    feat: add material template to dashboard

--- EquiSurveyAI/src/app/app.component.ts
+++ EquiSurveyAI/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { DashComponent } from './dashboard/dash/dash.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, DashComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ EquiSurveyAI/src/app/dashboard/dash/dash.component.ts
@@ -0,0 +1,39 @@
+import { Component, inject } from '@angular/core';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
+import { AsyncPipe } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-dash',
+  templateUrl: './dash.component.html',
+  styleUrl: './dash.component.scss',
+  standalone: true,
+  imports: [
+    AsyncPipe,
+    MatGridListModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
+  ]
+})
+export class DashComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
+  /** Based on the screen size, switch from standard to one column per row */
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+
+      return [
+        { title: 'Recent Surveys', cols: 2, rows: 1 },
+        { title: 'Quick Stats', cols: 1, rows: 1 },
+        { title: 'Notifiacations', cols: 1, rows: 2 },
+      ];
+    })
+  );
+}
